b84a2acd78f486752a9960249c0897997fd59856
Pshriva5 <pshriva5@asu.edu>
2018-02-12 23:38:00 -0700

    User Stories 4,7 Task 1 javascript for date filtering and display on Events UI

--- service/public/angularJS/events.js
+++ service/public/angularJS/events.js
@@ -1,48 +1,90 @@
 console.log("I am in AngularController")
 var events = angular.module('events',[]);
     events.controller('events', function($scope, $http){
-      // $scope.events = [{name : "ASU Tempe Trekking", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "ASU Trekking at Arizona State University at A Mountain Tempe ASU", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "San Jose Marathon", image : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg", description : "San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "Phoenix Rowing", image : "https://fwmdocks.com/wp-content/uploads/2017/01/FWM-Rowing-807x356-1.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "Oregon Rowing", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown MarathonSan Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "Seattle Marathon", image : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "San Jose Marathon", image : "https://fwmdocks.com/wp-content/uploads/2017/01/FWM-Rowing-807x356-1.jpg", description : "SASU Trekking at Arizona State UniversityASU Trekking at Arizona State UniversityASU Trekking at Arizona State University", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://alhujjattravels.com/wp-content/uploads/2017/04/treking-1.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "San Jose Marathon", image : "http://thehotzoneusa.com/wp-content/uploads/2014/11/14803118587_20f2a571fc_o.jpg", description : "ASU Trekking at Arizona State University at A Mountain Tempe ASUASU Trekking at Arizona State University at A Mountain Tempe ASU", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "ASU Tempe Trekking", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "San Jose Marathon", image : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "http://thehotzoneusa.com/wp-content/uploads/2014/11/14803118587_20f2a571fc_o.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "Arizona Trekking", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "ASU Tempe Trekking", image : "https://fwmdocks.com/wp-content/uploads/2017/01/FWM-Rowing-807x356-1.jpg", description : "ASU Trekking at Arizona State University at A Mountain Tempe ASU Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "ASU Tempe Trekking", image : "https://alhujjattravels.com/wp-content/uploads/2017/04/treking-1.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "San Jose Marathon", image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon", image : "http://thehotzoneusa.com/wp-content/uploads/2014/11/14803118587_20f2a571fc_o.jpg", description : "San Jose Downtown Marathon", date : "December 21 2018", location : "A-Mountain", time : "03:00 pm - 06:00 pm"},
-      //   {name : "Seattle Marathon", image : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg", description : "San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon San Jose Downtown Marathon ", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"},{name : "San Jose Marathon" , image : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg", description : "San Jose Downtown Marathon", date : "March 21 2018", location : "DownTown San Jose", time : "03:00 pm - 06:00 pm"}];
-
-        // $http({
-        //     method : "GET",
-        //     url : '/getAllEvents',//change the method to get 10 items at a time.
-        //     data : {}
-        // }).success(function(data) {
-        //     console.log("inside success");
-        //     console.log("data is ::");
-        //     console.log(data);
-
-        //     $scope.events = data;
-        //     //set all variables.
-                 
-        // }).error(function(error) {
-        //     console.log("inside error");
-        //     console.log(error);
-        //     $scope.unexpected_error = false;
-        //     $window.alert("unexpected_error");
-        // });
+      //START: As soon as events page loads
         $http.get('/getAllEvents').then(successCallback, errorCallback);
 
         function successCallback(response){
             //success code
-            console.log("In success of frontend after the route js call")
+            console.log("Successfully returning to frontend JS after loading all events")
             console.log(response.data)
-            $scope.events = response.data;
+            getFutureEvents(response.data)
+            $scope.allEvents = response.data;
         }
+        //while error from backend
         function errorCallback(error){
-            //error code
+            console.log("Error loading all events")
             console.log("In error")
         }
+        
+        function getFutureEvents(allEvents){
+          console.log("Loading upcoming events")
+          var upcomingEvents = [];
+          var k = 0;
+          var currentDate = new Date();
+          currentDate.setHours(0,0,0,0);
+          for(i = 0; i<allEvents.length; i++){
+            var eventDate = new Date(allEvents[i].date);
+            eventDate.setHours(0,0,0,0);
+            if(eventDate.getTime() >= currentDate.getTime()){
+              upcomingEvents[k++] = allEvents[i]
+            }
+          }
+          formatDate(upcomingEvents);
+          $scope.events = upcomingEvents;
+        }
+
+        //Upcoming events
+        $scope.getUpcomingEvents = function() {
+          getFutureEvents($scope.allEvents);
+        }
+
+        // Past events
+        $scope.getPastEvents = function() {
+          console.log("Loading past events");
+          var pastEvents = [];
+          var k = 0;
+          var currentDate = new Date();
+          currentDate.setHours(0,0,0,0);
+          for(i = 0; i<$scope.allEvents.length; i++){
+            var eventDate = new Date($scope.allEvents[i].date);
+            eventDate.setHours(0,0,0,0);
+            if(eventDate.getTime() < currentDate.getTime()){
+              pastEvents[k++] = $scope.allEvents[i]
+            }
+          }
+          formatDate(pastEvents);
+          $scope.events = pastEvents;
+        }
+
+
+        function formatDate(eventsList){
+            //success code
+            console.log("Formatting dates, list length: " + eventsList.length);
+            var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+            for(i = 0; i<eventsList.length; i++){
+              var eventDate = new Date(eventsList[i].date);
+              eventDate.setHours(0,0,0,0)
+              var setDate = monthNames[eventDate.getMonth()] + ' ' + eventDate.getDate() + ' ' + eventDate.getFullYear();
+              eventsList[i].date = setDate;
+            }
+        }
+       
+
+        // function formatDate(eventsList){
+        //     //error code
+        //     for(int i = 0; i<5;i++){
+        //       console.log(eventsList[i])
+        //     }
+        // }
+        // var d = new Date("02-10-2018");
+        // var e = new Date();
+        // d.setHours(0,0,0,0);
+        // e.setHours(0,0,0,0);
+        // console.log(d);
+        // console.log(e);
+        // if(d.getTime() == e.getTime()){
+        //   console.log("Same dates");
+        // }else{console.log("Different dates");}
     });
