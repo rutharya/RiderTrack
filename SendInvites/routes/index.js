62fdae83fb2b1140f87c70e0c9e3ae0a27720522
Rutuja Faldu <rutujafaldu@Rutujas-MacBook-Pro.local>
2018-02-22 21:23:23 -0700

    Sending invites via Text Message using Twilio

--- SendInvites/routes/index.js
+++ SendInvites/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var twilio = require('twilio');
+var client = new twilio('ACb220b8488fec4b587dccd0ede4f2e034', 'a0a8ee8f0eabce9da586beb75de91217');
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -9,6 +11,7 @@ var transporter = nodemailer.createTransport({
         pass: 'ridertrack5'
     }
 });
+
 
 
 /* GET home page. */
@@ -32,5 +35,14 @@ router.post('/send', function(req, res, next) {
     });
     //res.render('index', { title: 'Rutuja' });
     res.send('Email Sent');
+});
+
+router.post('/sendText', function(req, res, next) {
+    client.messages.create({
+        to: '+14809306564',
+        from: '+14438562291 ',
+        body: 'Welcome to Rider Track Event'
+    });
+    res.send('SMS sent');
 });
 module.exports = router;
