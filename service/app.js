a18589c850d7f38ea9ac5865269cdc0fec9d9459
Rutuja Faldu <rutujafaldu@Rutujas-MacBook-Pro.local>
2018-02-22 17:51:21 -0700

    Task#20 Tracking page UI changes and fixing CORS bug

--- service/app.js
+++ service/app.js
@@ -36,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
   next();
 });
 app.use(require('express-session')({ secret: 'ridertrack app', resave: true, saveUninitialized: true }));
