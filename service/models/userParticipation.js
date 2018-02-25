10f940cffa263b033cd54b4b6953acb4930e359f
Pshriva5 <pshriva5@asu.edu>
2018-02-25 15:19:49 -0700

    Task #40 userParticipation schema to track multiple atheletes

---
+++ service/models/userParticipation.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userParticipationSchema = new Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Rider'
+    },
+    events:[{event_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+      }],
+    current_race:[{
+      event_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+      }
+      lat : Number,
+      long : Number,
+      timestamp: Date,
+      speed: Number,
+      distLeft: Number,
+      altitude: Number
+    }],
+    recentEvents: [{
+      //Decide the granulaity and then use this for live tracking
+      event_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+      }
+      lat : Number,
+      long : Number,
+      timestamp: Date,
+    }]
+});
+
+var userParticipation = mongoose.model('userParticipation', userParticipationSchema);
+module.exports = UserParticipation;
