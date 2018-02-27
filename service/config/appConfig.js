04fb21c87f3c3692c88190c0e3d36359e5de449a
rmarya-asu <ruthvik.arya@asu.edu>
2018-02-27 03:18:16 -0700

    changing redis url

--- service/config/appConfig.js
+++ service/config/appConfig.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 if(env === 'production'){
   process.env.MONGODB_URI = 'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PWD+'@cluster0-shard-00-00-tjizg.mongodb.net:27017,cluster0-shard-00-01-tjizg.mongodb.net:27017,cluster0-shard-00-02-tjizg.mongodb.net:27017/ridertrack?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
-  process.env.REDIS_URI = 'redis-13189.c15.us-east-1-2.ec2.cloud.redislabs.com:13189';
+  process.env.REDIS_URI = 'redis://redis-13189.c15.us-east-1-2.ec2.cloud.redislabs.com:13189';
 }
 else if (env === 'development') {
     //local env
