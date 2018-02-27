f2da8aa96d98ed5c76d8fa2a7ec207bd301b2270
rmarya-asu <ruthvik.arya@asu.edu>
2018-02-27 03:12:00 -0700

    forgot to add appConfig changes

--- service/config/appConfig.js
+++ service/config/appConfig.js
@@ -1,11 +1,13 @@
 var env = process.env.NODE_ENV || 'development';
 if(env === 'production'){
   process.env.MONGODB_URI = 'mongodb://'+process.env.MONGO_USER+':'+process.env.MONGO_PWD+'@cluster0-shard-00-00-tjizg.mongodb.net:27017,cluster0-shard-00-01-tjizg.mongodb.net:27017,cluster0-shard-00-02-tjizg.mongodb.net:27017/ridertrack?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+  process.env.REDIS_URI = 'redis-13189.c15.us-east-1-2.ec2.cloud.redislabs.com:13189';
 }
 else if (env === 'development') {
     //local env
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost/ridertrack';
+    process.env.REDIS_URI = 'redis://localhost:6379';
 }
 module.exports = {
   environment:env,
