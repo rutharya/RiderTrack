868373a745d833818a8a87b48e3d9cc107c1be10
Pshriva5 <pshriva5@asu.edu>
2018-03-16 03:25:16 -0700

    Task #78 Events page Angular 4 conversion and few refactorings

--- rt-angular/src/app/events/events.component.ts
+++ rt-angular/src/app/events/events.component.ts
@@ -10,6 +10,47 @@ import {EventsService} from "../shared/services/events.service";
 
 export class EventsComponent implements OnInit {
   asdf = '';
+  
+
+
+
+events = [
+    {
+        "name" : "Seattle Marathon",
+        "image" : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg",
+        "description" : "ASU Trekking at Arizona State University at A Mountain Tempe ASU",
+        "date" : "02-10-2017",
+        "location" : "Washington Street",
+        "time" : "03:00 pm - 06:00 pm"
+    },
+    {
+        "name" : "ASU Tempe Trekking",
+        "image" : "https://s-media-cache-ak0.pinimg.com/originals/48/51/6a/48516a2b7d82b4755063e4c737d764b2.jpg",
+        "description" : "ASU Trekking at Arizona State University at A Mountain Tempe ASU",
+        "date" : "02-10-2017",
+        "location" : "A-Mountain",
+        "time" : "03:00 pm - 06:00 pm"
+    },
+    {
+        "name" : "Event TWO",
+        "image" : "https://fwmdocks.com/wp-content/uploads/2017/01/FWM-Rowing-807x356-1.jpg",
+        "description" : "ASU Trekking at Arizona State University at A Mountain Tempe ASU",
+        "date" : "02-10-2017",
+        "location" : "A-Mountain",
+        "time" : "03:00 pm - 06:00 pm"
+    },
+    {
+        "name" : "Event THREE",
+        "image" : "http://www.parcjeandrapeau.com/medias/images/header/marathon-et-demi-marathon-oasis-rock-n-roll-de-montreal.jpg",
+        "description" : "ASU Trekking at Arizona State University at A Mountain Tempe ASU",
+        "date" : "02-10-2017",
+        "location" : "A-Mountain",
+        "time" : "03:00 pm - 06:00 pm"
+    }
+
+    ]
+
+
 
   constructor(private eventsService: EventsService){
 
@@ -18,6 +59,7 @@ export class EventsComponent implements OnInit {
   ngOnInit() {
     this.eventsService.getEvents()
       .subscribe(res => {
+
         console.log(res);
         this.asdf = res.toString();
       });
