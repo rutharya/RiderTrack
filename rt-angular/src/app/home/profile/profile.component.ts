27bc000a81b49012e940b5ffbe24ae12eaba810b
midhunlingam <mlingam@asu.edu>
2018-03-17 15:58:46 -0700

    US 3 - Manage Accounts - #82 - Profile page front end in angular

--- rt-angular/src/app/home/profile/profile.component.ts
+++ rt-angular/src/app/home/profile/profile.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {UserService} from "../../shared/services";
-import {User} from "../../shared/models";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../../shared/services';
+import {User} from '../../shared/models';
+import {HeaderComponent} from '../../shared/layout';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +11,27 @@ import {User} from "../../shared/models";
 })
 export class ProfileComponent implements OnInit {
   currentUser: User;
+  isSubmitting: boolean;
+  EDL: string;
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.currentUser = this.userService.getCurrentUser()
+    this.currentUser = this.userService.getCurrentUser();
+    this.isSubmitting = false;
+    this.EDL = 'Edit';
   }
 
+  submitForm() {
+
+  }
+
+  enableEdit() {
+    if (this.isSubmitting === true) {
+      this.isSubmitting = false;
+      this.EDL = 'Lock';
+    } else {
+      this.EDL = 'Edit';
+      this.isSubmitting = true;
+    }
+  }
 }
