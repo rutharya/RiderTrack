919a794a76c58a823868f903339835f951a87ada
midhunlingam <mlingam@asu.edu>
2018-03-18 17:25:41 -0700

    US 3 - Manage Accounts - #82 - Profile page front end in angular

--- rt-angular/src/app/app.module.ts
+++ rt-angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {LoginModule} from './login/login.module';
 import { HomeModule } from './home/home.module';
-import {ReactiveFormsModule} from '@angular/forms';
+import {Validators,ReactiveFormsModule} from '@angular/forms';
 
 
 import {
@@ -56,7 +56,8 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([
     rootRouting,
     SharedModule,
     TrackingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Validators
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true},
     ApiService,
