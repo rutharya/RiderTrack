3ae02e381d169b034b31ccb2e698ada0c7a06b03
midhunlingam <mlingam@asu.edu>
2018-03-22 14:49:18 -0700

    US3 #101 Tests of User Input from the profile page

--- rt-angular/src/app/home/profile/profile.component.ts
+++ rt-angular/src/app/home/profile/profile.component.ts
@@ -15,8 +15,17 @@ export class ProfileComponent implements OnInit {
   currentUser: User;
   EDL: string;
   details: FormGroup;
+  fname: string;
+  lname: string;
+  height: number;
+  weight: number;
   phonenumber: string;
   email: string;
+  address: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: string;
   password: string;
   passwordconf: string;
 
@@ -29,28 +38,25 @@ export class ProfileComponent implements OnInit {
     this.EDL = 'Edit';
     this.details = new FormGroup({
       image: new FormControl(),
-      bio: new FormControl(),
+      firstname: new FormControl(this.fname, [Validators.required]),
+      lastname: new FormControl(this.lname, [Validators.required]),
+      height: new FormControl(this.height, [Validators.required]),
+      weight: new FormControl(this.weight, [Validators.required]),
+      phonenumber: new FormControl(this.phonenumber, [Validators.required, Validators.pattern('[0-9]{10}')]),
       email: new FormControl(this.email, [Validators.required, Validators.email]),
+      address: new FormControl(this.address, [Validators.required]),
+      address2: new FormControl(),
+      city: new FormControl(this.city, [Validators.required]),
+      state: new FormControl(this.state, [Validators.required]),
+      zipcode: new FormControl(this.zipcode, [Validators.required]),
+      bio: new FormControl(),
       password: new FormControl(this.password, [Validators.required, Validators.pattern(
         '^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
-      passwordconf: new FormControl(this.passwordconf, [Validators.required, matchOtherValidator('password')]),
-      firstname: new FormControl(),
-      lastname: new FormControl(),
-      height: new FormControl(),
-      weight: new FormControl(),
-      phonenumber: new FormControl(this.phonenumber, [Validators.required, Validators.pattern('[0-9]{10}')]),
-      address: new FormControl(),
-      address2: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      zipcode: new FormControl()
+      passwordconf: new FormControl(this.passwordconf, [Validators.required, matchOtherValidator('password')])
     });
-
-
 
     this.details.disable();
   }
-
 
 
   submitForm() {
