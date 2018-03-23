9e5bfc500c90fb58708841bf2cde55c4aa65f7ce
rutujafaldu94 <rfaldu@asu.edu>
2018-03-23 12:43:33 -0700

    Task 21: setting up leaflet and omnivore

--- rt-angular/src/app/tracking/tracking.component.ts
+++ rt-angular/src/app/tracking/tracking.component.ts
@@ -3,23 +3,25 @@ import {LatestLocationService} from './latest-location.service';
 import {TrackingData} from '../shared/models/trackingData';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-tracking',
   templateUrl: './tracking.component.html',
   styleUrls: ['./tracking.component.css']
 })
+
+
 export class TrackingComponent implements OnInit {
   locationData$: Observable<Array<TrackingData>>;
+  apiToken: any;
 
-  constructor(private latestLocationService: LatestLocationService) { }
-  //constructor(private http: HttpClient) { }
+  constructor(private latestLocationService: LatestLocationService) {}
 
   ngOnInit() {
-    //this.latestLocationService.getLatestLocation().subscribe(
-     // trackingData => this.trackingDataArray = trackingData);
-    //this.latestLocationService.getLatestLocation();
+    this.apiToken = environment.MAPBOX_API_KEY;
     this.getLatestLocation();
+    this.latestLocationService.plot(this.locationData$);
   }
 
   getLatestLocation(): void {
