a087d2a5c08076eeda2ca6ae4b1b403b9cb8ddcf
rmarya-asu <ruthvik.arya@asu.edu>
2018-04-08 14:05:53 -0700

    Taiga Issue 158 : PPE BUild broken. Add firebase to environment (prod)
    https://tree.taiga.io/project/pshriva5-ridertrack-1/issue/158

--- rt-angular/src/environments/environment.prod.ts
+++ rt-angular/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   api_url: '',
-  MAPBOX_API_KEY: 'pk.eyJ1IjoicnV0dWphZmFsZHUiLCJhIjoiY2pmMWo0ZWpsMDBhNTJ3cGRxYW5meXgyaCJ9.xBm2FFUhUjaEUtCVwLJKAw'
+  MAPBOX_API_KEY: 'pk.eyJ1IjoicnV0dWphZmFsZHUiLCJhIjoiY2pmMWo0ZWpsMDBhNTJ3cGRxYW5meXgyaCJ9.xBm2FFUhUjaEUtCVwLJKAw',
+  firebase: {
+    apiKey: 'AIzaSyAa1P1MdU5xs6oRzLtdGFh3WOVn_N3bUWs',
+    authDomain: 'ridertrack-c90b1.firebaseapp.com',
+    databaseURL: 'https://ridertrack-c90b1.firebaseio.com',
+    projectId: 'ridertrack-c90b1',
+    storageBucket: 'ridertrack-c90b1.appspot.com',
+    messagingSenderId: '1018106080097'
+  }
 };
