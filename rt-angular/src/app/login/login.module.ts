44e3f12d569d354c163ece56e8bebe65b5ab719d
Ruthvik Arya <rmarya@asu.edu>
2018-04-20 09:55:02 -0400

    build fix: (heroku threw error since one commit is missing)

--- rt-angular/src/app/login/login.module.ts
+++ rt-angular/src/app/login/login.module.ts
@@ -5,7 +5,7 @@ import { NoAuthGuard } from './no-auth-guard.service';
 import { SharedModule } from '../shared';
 import {LoginComponent} from "./login.component";
 import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
-import {ForgotPwdComponent2} from "./forgot-pwd2/forgot-pwd.component2";
+// import {ForgotPwdComponent2} from "./forgot-pwd2/forgot-pwd.component2";
 
 const authRouting: ModuleWithProviders = RouterModule.forChild([
   {
@@ -23,11 +23,11 @@ const authRouting: ModuleWithProviders = RouterModule.forChild([
     component: ForgotPwdComponent,
     canActivate: [NoAuthGuard]
   },
-  {
-    path:'forgotpwd/:id',
-    component: ForgotPwdComponent2,
-    canActivate: [NoAuthGuard]
-  }
+  // {
+  //   path:'forgotpwd/:id',
+  //   component: ForgotPwdComponent2,
+  //   canActivate: [NoAuthGuard]
+  // }
 ]);
 
 @NgModule({
@@ -38,7 +38,7 @@ const authRouting: ModuleWithProviders = RouterModule.forChild([
   declarations: [
     LoginComponent,
     ForgotPwdComponent,
-    ForgotPwdComponent2
+    // ForgotPwdComponent2
   ],
 
   providers: [
