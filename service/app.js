c72480270043d5e0d05c6bd9087789fc6463f0da
rmarya-asu <ruthvik.arya@asu.edu>
2018-04-26 00:12:05 -0700

    task #87: refactor service code
    adding error handlers.

--- service/app.js
+++ service/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var chalk = require('chalk');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
@@ -55,6 +56,10 @@ app.get('*', function (req, res, next) {
   res.sendFile(path.resolve('./service/dist/index.html'));
 });
 
+
+//ruthar: this code is commented out since we are handling errors form the service via the error handler below,
+//and serving /index.html or error routes from angular.
+//ruthar: Please dont remove the commented out lines as this can be used as reference. 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   var err = new Error('Not Found');
@@ -64,13 +69,14 @@ app.get('*', function (req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.log(req.body);
+  console.log(chalk.red('error handler in app.js called'));
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+    // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({result:false,status: {msg: err}});
 });
 
 
